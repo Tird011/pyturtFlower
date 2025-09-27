c07aaca2ff154e007ab1386bdd079b5c4a90d5f2
Tird <3rdrecintolego@gmail.com>
2025-09-27 23:46:34 +0800

    Update weird.py

--- PyTurt/weird.py
+++ PyTurt/weird.py
@@ -1,14 +1,16 @@
 from turtle import *
+
 speed(0)
 bgcolor("black")
-colors = ['red' , 'violet']
+colors = ["orange", "white"]
 hideturtle()
-for i in range(222):
-goto(0,0)
-color(colors[i % 2])
-foward(130)
-left (3)
-circle(40)
-foward(130)
-right(180)
+for i in range(122):
+  goto(0,0)
+  color(colors[i%2])
+  forward(130)
+  left(3)
+  circle(40)
+  forward(130)
+  right(180)
+
 done()
