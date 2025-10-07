2e7615920f6400080f9d9c7351d1eba081c93c9f
Tird <3rdrecintolego@gmail.com>
2025-10-07 23:08:37 +0800

    Update antiSort.py

--- Junks/antiSort.py
+++ Junks/antiSort.py
@@ -2,7 +2,9 @@ a = [8,6,7,2,9,3]
 
 print(a)
 for i in range(0,len(a)):
-  if a[i] >= a[j]:
-    temp = a[i]
-    a[j] = temp
-    print(a)
+    for j in range(i,len(a)):
+        if a[i] >= a[j]:
+            temp = a[i]
+            a[i] = a[j]
+            a[j] = temp
+print(a)
