26ce2c93dba8583ffd2c372b91e22ea1b7f070c6
Tird <3rdrecintolego@gmail.com>
2025-10-26 21:34:17 +0800

    Create oops.py

---
+++ Junks/oops.py
@@ -0,0 +1,11 @@
+class main:
+    def __init__(user,name,age)
+        user.name = name 
+        user.age = age
+    
+    def user_name(user):
+        print(f"I am {user.name}")       
+        print(f"and lived for {user.age} years")
+    
+println = main("jerricky",67 )
+println.user_name()
