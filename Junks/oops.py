67b7722d5d7ffa5824b1ff2ea766623e415ae42e
Tird <3rdrecintolego@gmail.com>
2025-10-26 21:56:00 +0800

    Update oops.py

--- Junks/oops.py
+++ Junks/oops.py
@@ -9,3 +9,23 @@ class main:
     
 println = main("jerricky",67 )
 println.user_name()
+
+#fun version or much more detailed
+
+class main:
+    def __init__(user,name,age):
+        user.name = name
+        user.age = age
+    
+    def user_name(user):
+        print(f"greetings I am {user.name}")
+        if user.age == 1:
+            print("{user.name} is a year old")
+        else:
+            print(f"I have lived for over {user.age}")
+    
+    def IWantIce(user):
+        print(f"{user.name} likes icecream")
+
+println = main("grogru",1) 
+println.user_name()
