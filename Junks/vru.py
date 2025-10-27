503e38ed7c46790b3fb44335d469fd5b5876bdc2
Tird <3rdrecintolego@gmail.com>
2025-10-27 22:11:44 +0800

    Update vru.py

--- Junks/vru.py
+++ Junks/vru.py
@@ -1,1 +1,14 @@
-///
+nom1 = 1 
+nom2 = 2
+
+def divisor(nom1,nom2):
+  sum = nom1/nom2*10
+
+def equalizer(sum,nom1):
+  if sum == 2:
+    sum / nom1
+  else:
+    print("aint no way")
+
+println = divisor(4,3)
+println.divisor()
