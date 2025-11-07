10222bb75f5a864b08d59c5b9134d60f3126e686
Tird <3rdrecintolego@gmail.com>
2025-11-07 23:48:21 +0800

    Update reversecurse.py

--- Junks/reversecurse.py
+++ Junks/reversecurse.py
@@ -1,1 +1,14 @@
-#a for loop on reverse on nov 7
+#negative = -1
+#positive = -1
+
+#def __init__ (negative,positive):
+#for i in negative += positive:
+    #sum = negative + positive
+    #if sum == 0:
+        #sum/10 * 1000
+        #return
+        #for sum * negative/positive:
+          #print("yey")
+    #else:
+        #print("lol")
+
