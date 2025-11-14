fff3debe773ee8d8b54d35a5d00c36dd6fce1fc3
Tird <3rdrecintolego@gmail.com>
2025-11-14 22:39:50 +0800

    Implement binPot function and initialization
    Fix the binPot function and add initialization logic.

--- Junks/pot.py
+++ Junks/pot.py
@@ -1,1 +1,15 @@
-#nov 10
+def binPot(num, target):
+  for num in target:
+    if num == 0:
+      return num
+
+return -1
+
+def __init__():
+  num[] = {1,2,3,4,5,}
+  target = 4
+  results = binPot(num,target)
+  if results == -1:
+    print(results)
+  else:
+    print("its not here")
